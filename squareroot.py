9e377d7fd61ac794e4e4189329406efe8b4b17c6
Aldas Žarnauskas <109503215+aldaszarnauskas@users.noreply.github.com>
2025-03-20 17:22:39 +0200

    Update squareroot.py

--- squareroot.py
+++ squareroot.py
@@ -1,13 +1,27 @@
 #!/bin/env python3
 
+# Prompt a user to input a positive integer
 aninput = input("Enter a positive number: ")
+# Convert the input to a float
 aninput = float(aninput)
+
+
+# Th formula to calculate the square root: 
 # root = 0.5 * (X + (N / X))
 # x = a for a > 0, i.e. to solve x2 = a
+
+# Define an x
 x = aninput
+# Define the # of counts for the while loop
 count = 0
 
+# Continue the while loop until either one of the conditions is False
 while ((x * x != aninput) and (count < 1000)):
-  x = 0.5 * (x + (aninput/x))
-  count += 1
-print(f"The square root of {aninput} is ≈ {round(x, 5)}.")
+    # Perform calculations based on the formula above
+    x = 0.5 * (x + (aninput/x))
+    
+    # Track the # of iterations of the while loop
+    count += 1
+    
+# Print the square root of the input
+print(f"The square root of {int(aninput)} is ≈ {round(x, 5)}.")
